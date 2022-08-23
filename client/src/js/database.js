6a63670a2b8677180d34bbb1b22ee13251ecb595
kitanablade <kiteagle@gmail.com>
2022-08-23 14:26:58 -0700

    working text editor with service worker and offline funct

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,7 +27,7 @@ const store = tx.objectStore('jate');
 
 //TODO: Ask if using .update instead of .add is the proper method here
 // Use the .add() method on the store and pass in the content.
-const request = store.add({ content: content });
+const request = store.put({ id: 1, value: content });
 
 // Get confirmation of the request.
 const result = await request;
@@ -48,7 +48,7 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,34 +1,52 @@
-const butInstall = document.getElementById('buttonInstall');
+// const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// DONE: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
-    // Store the triggered events
-    window.deferredPrompt = event;
+// // Logic for installing the PWA
+// // TODO: Add an event handler to the `beforeinstallprompt` event
+// window.addEventListener('beforeinstallprompt', (event) => {
+//     // Store the triggered events
+//     window.deferredPrompt = event;
 
-    // Remove the hidden class from the button.
-    butInstall.classList.toggle('hidden', false);
-});
+//     // Remove the hidden class from the button.
+//     butInstall.classList.toggle('hidden', false);
+// });
 
-// DONE: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
+// // TODO: Implement a click event handler on the `butInstall` element
+// butInstall.addEventListener('click', async () => {
+//     const promptEvent = window.deferredPrompt;
 
-    if (!promptEvent) {
-     return;
-    }
+//     if (!promptEvent) {
+//      return;
+//     }
   
-    // Show prompt
-    promptEvent.prompt();
+//     // Show prompt
+//     promptEvent.prompt();
     
-    // Reset the deferred prompt variable, it can only be used once.
-    window.deferredPrompt = null;
+//     // Reset the deferred prompt variable, it can only be used once.
+//     window.deferredPrompt = null;
     
-    butInstall.classList.toggle('hidden', true);
-});
+//     butInstall.classList.toggle('hidden', true);
+// });
 
-// DONE: Add an handler for the `appinstalled` event
+// // TODO: Add an handler for the `appinstalled` event
+// window.addEventListener('appinstalled', (event) => {
+//     // Clear prompt
+//   window.deferredPrompt = null;
+// });
+
+const butInstall = document.getElementById('buttonInstall');
+window.addEventListener('beforeinstallprompt', (event) => {
+  window.deferredPrompt = event;
+  butInstall.classList.toggle('hidden', false);
+});
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
+  promptEvent.prompt();
+  window.deferredPrompt = null;
+  butInstall.classList.toggle('hidden', true);
+});
 window.addEventListener('appinstalled', (event) => {
-    // Clear prompt
   window.deferredPrompt = null;
 });
